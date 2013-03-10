d8e285d8a07d5591173252303c7eaac842658f10
josephbulger <joseph@josephbulger.com>
2013-03-10 19:22:02 -0400

    refactoring for next graph input test case

--- trainteaser.tests/RouteDistanceTests.cs
+++ trainteaser.tests/RouteDistanceTests.cs
@@ -10,17 +10,18 @@ namespace trainteaser.tests
     [TestFixture]
     public class RouteDistanceTests
     {
+        [TestCase("Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7", 9)]
         [Test]
-        public void WhatIsTheDistance_ForRoute_AtoBtoC()
+        public void WhatIsTheDistance_ForRoute_AtoBtoC(string graphInput, int expectation)
         {
             //arrange
-            var graph = new Graph("Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7");
+            var graph = new Graph(graphInput);
 
             //act
             var foundRoute = new RouteFinder(graph).FindRoute(new RouteRequest("A", "B", "C"));
 
             //assert
-            Assert.That(foundRoute.Distance, Is.EqualTo(9));
+            Assert.That(foundRoute.Distance, Is.EqualTo(expectation));
         }
         
     }
