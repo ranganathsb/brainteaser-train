1d7df2cda3a69a961c56426301ecbd57118d24a6
josephbulger <joseph@josephbulger.com>
2013-03-10 19:07:02 -0400

    first try to figure out the route from a to b to c for a given graph input

--- trainteaser.tests/RouteDistanceTests.cs
+++ trainteaser.tests/RouteDistanceTests.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace trainteaser.tests
 {
+    [TestFixture]
     public class RouteDistanceTests
     {
+        [Test]
+        public void WhatIsTheDistance_ForRoute_AtoBtoC()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+        
     }
 }
