2c0a7062bc4407ccceec62e2a423a66cc6624809
josephbulger <joseph@josephbulger.com>
2013-03-10 22:07:06 -0400

    building test for finding shortest route from a to c, first test case

---
+++ trainteaser.tests/ShortestRouteTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using trainteaser.Route;
+
+namespace trainteaser.tests
+{
+    [TestFixture]
+    public class ShortestRouteTests
+    {
+        [Test]
+        public void WhatIsThe_ShortestRoute_FromAtoC()
+        {
+            //arrange
+            var graph = new Graph("Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7");
+
+            //act
+            var routeResponse = new ShortestRouteFinder(graph).FindRoute('A', 'C');
+
+            //asset
+            Assert.That(routeResponse.GetResponse(), Is.EqualTo(9.ToString()));
+        }
+         
+    }
+}
---
+++ trainteaser/Route/ShortestRouteFinder.cs
@@ -0,0 +1,15 @@
+﻿namespace trainteaser.Route
+{
+    public class ShortestRouteFinder
+    {
+        public ShortestRouteFinder(Graph graph)
+        {
+            
+        }
+
+        public RouteResponse FindRoute(char startingTown, char endingTown)
+        {
+            return new RouteResponse {Distance = 9};
+        }
+    }
+}
