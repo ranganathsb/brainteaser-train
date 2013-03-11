28326dec67b9736933a57b184457e6b4b8231ae0
josephbulger <joseph@josephbulger.com>
2013-03-10 20:49:10 -0400

    refactoring

--- trainteaser.tests/TripTests.cs
+++ trainteaser.tests/TripTests.cs
@@ -5,17 +5,18 @@ namespace trainteaser.tests
     [TestFixture]
     public class TripTests
     {
+        [TestCase("Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7", 2)]
         [Test]
-        public void WhatAreTheNumberOfTrips_StartingAtCAndEndingAtC_WithAMaximumOfThreeHops()
+        public void WhatAreTheNumberOfTrips_StartingAtCAndEndingAtC_WithAMaximumOfThreeHops(string graphInput, int expectation)
         {
             //arrange
-            var graph = new Graph("Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7");
+            var graph = new Graph(graphInput);
 
             //act
             var tripResponse = new TripFinder(graph).FindTrip('C', 'C');
 
             //assert
-            Assert.That(tripResponse.NumberOfTrips, Is.EqualTo(2));
+            Assert.That(tripResponse.NumberOfTrips, Is.EqualTo(expectation));
         }
          
     }
