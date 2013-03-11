acff9bb3158b2b978826b1c9230b1d211120a840
josephbulger <joseph@josephbulger.com>
2013-03-10 21:54:27 -0400

    adding missing refactored files

---
+++ trainteaser/ExactStopTripFinder.cs
@@ -0,0 +1,37 @@
+﻿namespace trainteaser
+{
+    public class ExactStopTripFinder: TripFinder
+    {
+        private int NumberOfStops { get; set; }
+
+        public ExactStopTripFinder(Graph graph) : base(graph)
+        {
+            NumberOfStops = 4;
+        }
+
+        public override TripResponse FindTrip(char startTown, char destination)
+        {
+            var response = new TripResponse();
+
+            FindTrips(new TripQuery {StartingTown = startTown, Desination = destination, Stops = 0, Response = response});
+
+            return response;
+        }
+
+        protected override void FindTrips(TripQuery query)
+        {
+            if (query.Stops > NumberOfStops)
+                return;
+
+            var countTheseTrips = query.Stops == NumberOfStops;
+
+            var routes = GetRoutesFromStartingTown(query.StartingTown);
+
+            query.Response.NumberOfTrips += countTheseTrips ? GetRoutesThatAreFinished(query.Desination, routes) : 0;
+
+            query.Stops++;
+
+            LookAtRoutesThatCouldWork(query.Desination, query.Stops, query.Response, routes);
+        }
+    }
+}
