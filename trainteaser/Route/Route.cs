0db5ff236a02cbb67f2af572f70afcd4d9522fbd
josephbulger <joseph@josephbulger.com>
2013-03-11 00:01:51 -0400

    implemented shortest parth alg to pass the 3 test cases i made

--- trainteaser/Route/Route.cs
+++ trainteaser/Route/Route.cs
@@ -9,5 +9,19 @@ namespace trainteaser.Route
         public int Distance { get; set; }
 
         public bool NoRouteFound { get; set; }
+
+        public bool Visited { get; set; }
+
+        public Route Parent { get; set; }
+
+        public int PathDistance
+        {
+            get
+            {
+                if (Parent != null)
+                    return Distance + Parent.PathDistance;
+                return Distance;
+            }
+        }
     }
 }
--- trainteaser/Route/ShortestRouteFinder.cs
+++ trainteaser/Route/ShortestRouteFinder.cs
@@ -1,15 +1,59 @@
-﻿namespace trainteaser.Route
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace trainteaser.Route
 {
     public class ShortestRouteFinder
     {
+        private Graph Graph { get; set; }
+
         public ShortestRouteFinder(Graph graph)
         {
-            
+            Graph = graph;
         }
 
         public RouteResponse FindRoute(char startingTown, char endingTown)
         {
-            return new RouteResponse {Distance = 9};
+            var routes = FindAllRoutes(startingTown, endingTown);
+
+            return new RouteResponse {Distance = routes.Min(x => x.PathDistance)};
+        }
+
+        private IList<Route> FindAllRoutes(char startingTown, char endingTown)
+        {
+            var routes = Graph.QueryRoutes().Where(x => x.StartingTown == startingTown);
+            
+            var queue = new Queue<Route>();
+            routes.ToList().ForEach(queue.Enqueue);
+
+            var result = new List<Route>();
+
+            while(queue.Count != 0)
+            {
+                var currentRoute = queue.Dequeue();
+                
+                currentRoute.Visited = true;
+
+                foreach (var child in GetRoutesChildren(currentRoute))
+                {
+                    child.Visited = true;
+
+                    child.Parent = currentRoute;
+
+                    if (child.EndingTown == endingTown)
+                        result.Add(child);
+                    else
+                        queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+        private IEnumerable<Route> GetRoutesChildren(Route currentRoute)
+        {
+            return Graph.QueryRoutes().Where(x => x.StartingTown == currentRoute.EndingTown && x.Visited == false);
         }
     }
 }
