e44dd0c9007c8846be828467ea7fc68ea1b4bbaa
josephbulger <joseph@josephbulger.com>
2013-03-10 22:09:30 -0400

    adding more test cases

--- trainteaser.tests/ShortestRouteTests.cs
+++ trainteaser.tests/ShortestRouteTests.cs
@@ -6,17 +6,20 @@ namespace trainteaser.tests
     [TestFixture]
     public class ShortestRouteTests
     {
+        [TestCase("Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7", 9)]
+        [TestCase("Graph: AB1, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7", 5)]
+        [TestCase("Graph: AB1, BC4, CD8, DC1, DE6, AD1, CE2, EB3, AE7", 2)]
         [Test]
-        public void WhatIsThe_ShortestRoute_FromAtoC()
+        public void WhatIsThe_ShortestRoute_FromAtoC(string graphInput, int expectation)
         {
             //arrange
-            var graph = new Graph("Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7");
+            var graph = new Graph(graphInput);
 
             //act
             var routeResponse = new ShortestRouteFinder(graph).FindRoute('A', 'C');
 
             //asset
-            Assert.That(routeResponse.GetResponse(), Is.EqualTo(9.ToString()));
+            Assert.That(routeResponse.GetResponse(), Is.EqualTo(expectation.ToString()));
         }
          
     }
