1decedadc0473fb2de21b7df717a1b6da7f217f3
josephbulger <joseph@josephbulger.com>
2013-03-11 20:53:12 -0400

    final output case

---
+++ trainteaser.tests/NumberOfRoutesTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace trainteaser.tests
+{
+    [TestFixture]
+    public class NumberOfRoutesTests
+    {
+        [Test]
+        public void HowManyDifferentRoutes_FromCtoC_WithADistanceLessThan30()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+         
+    }
+}
